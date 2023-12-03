a805e77a49d2e1bb56b401c12149b0536dc3a101
Ariel Ortiz <arielortiz.dev@gmail.com>
2023-12-02 20:05:35 -0800

    Initial Commit

---
+++ TimeManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimeManager
+//
+//  Created by Ariel Ortiz on 12/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimeManager/TimeManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeManagerApp.swift
+//  TimeManager
+//
+//  Created by Ariel Ortiz on 12/2/23.
+//
+
+import SwiftUI
+
+@main
+struct TimeManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
