c09ff90c005b90becf95131ee6127b7db077273d
Ariel Ortiz <arielortiz.dev@gmail.com>
2023-12-12 15:36:59 -0800

    Initial commit

--- TimeManager/ContentView.swift
+++ TimeManager/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+        
+    @StateObject var timeMN = TimeManager()
+    
+    private var dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm:ss"
+        return dateFormatter
+    }()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack{
+                Text("Time counter:")
+                Text(timeMN.timeCounter, formatter: dateFormatter)
+                Spacer()
+            }
+            HStack{
+                Text("Start time:")
+                Text(timeMN.startTime, formatter: dateFormatter)
+                Spacer()
+            }
+            HStack{
+                Text("End time:")
+                Text(timeMN.endTime, formatter: dateFormatter)
+                Spacer()
+            }
+            HStack{
+                Text("Rule time seconds:")
+                Text("\(timeMN.ruleTimeSeconds)")
+                Spacer()
+            }
+            HStack{
+                Text("Rule time extra seconds:")
+                Text("\(timeMN.ruleTimeExtraSeconds)")
+                Spacer()
+            }
         }
         .padding()
+        .onAppear{
+            timeMN.start(endTimeSeconds: 30, ruleTimeSeconds: 10, ruleTimeExtraSeconds: 20)
+        }
+        .onReceive(timeMN.timer) { _ in
+            timeMN.track()
+            
+        }
+        
     }
 }
 
---
+++ TimeManager/TimeMN.swift
@@ -0,0 +1,64 @@
+//
+//  TimeManager.swift
+//  TimeManager
+//
+//  Created by Ariel Ortiz on 12/2/23.
+//
+
+import Foundation
+
+final class TimeManager: ObservableObject{
+        
+    @Published var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    init(){
+        timer.upstream.connect().cancel()
+    }
+    
+    var calendar: Calendar {
+        let userLocale = Locale.autoupdatingCurrent
+        var gregorianCalendar = Calendar(identifier: .gregorian)
+        gregorianCalendar.locale = userLocale
+        return gregorianCalendar
+    }
+    
+    @Published var timeCounter: Date = .now
+    @Published var startTime: Date = .now
+    @Published var endTime: Date = .now
+    @Published var ruleTimeSeconds: Double = 0.0
+    @Published var ruleTimeExtraSeconds: Double = 0.0
+    
+    /// Set everything up to start tracking
+    /// - Parameters:
+    ///   - startTime: The started time (default: .now)
+    ///   - endTimeSeconds: The end time in seconds
+    ///   - ruleTimeSeconds: The time in seconds in which the extra seconds must be added
+    ///   - ruleTimeExtraSeconds: The amount of that time in seconds to add
+    func start(startTime: Date = .now, endTimeSeconds: Double, ruleTimeSeconds: Double = 0.0, ruleTimeExtraSeconds: Double = 0.0){
+        self.startTime = startTime
+        self.ruleTimeSeconds = ruleTimeSeconds
+        self.ruleTimeExtraSeconds = ruleTimeExtraSeconds
+        
+        let components = calendar.dateComponents([.year, .month, .day, .hour, .minute, .second], from: Date())
+        self.startTime = calendar.date(from: components)!
+        self.endTime = startTime.addingTimeInterval(endTimeSeconds)
+        timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    }
+    
+    /// Track the time
+    func track(){
+        self.timeCounter = Date()
+        // Check the distance to the end time
+        let currentTime = Date().distance(to: endTime)
+        if currentTime <= 0{
+            timer.upstream.connect().cancel()
+            return
+        }
+        
+        // Check if the currentTime is minor to the ruleTimeSeconds to add the ruleTimeExtraSeconds
+        if Int(currentTime) <= Int(ruleTimeSeconds){
+            endTime = endTime.addingTimeInterval(ruleTimeExtraSeconds)
+        }
+    }
+    
+}
